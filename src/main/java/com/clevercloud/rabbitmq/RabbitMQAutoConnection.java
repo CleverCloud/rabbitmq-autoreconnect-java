52bc4423d70f4613fa6e69856217c3cfff8a46af
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-08-14 14:59:44 +0200

    wrap around all Connection methods
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/main/java/com/clevercloud/rabbitmq/RabbitMQAutoConnection.java
+++ src/main/java/com/clevercloud/rabbitmq/RabbitMQAutoConnection.java
@@ -7,7 +7,10 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -50,8 +53,6 @@ public class RabbitMQAutoConnection {
       this.tries = tries;
 
       this.random = new Random();
-
-      this.checkConnection();
    }
 
    public RabbitMQAutoConnection(@Nonnull @NonEmpty List<String> hosts, String login, String password, long interval, long tries) throws IOException {
@@ -67,6 +68,9 @@ public class RabbitMQAutoConnection {
    }
 
    public void checkConnection() throws IOException {
+      if (this.isConnected())
+         return;
+
       for (int tries = 0; (this.tries == NO_TRIES_LIMIT || tries < this.tries) && !this.isConnected(); ++tries) {
          Logger.getLogger(RabbitMQAutoConnection.class.getName()).info("Attempting to " + ((this.connection != null) ? "re" : "") + "connect to the rabbitmq server.");
 
@@ -76,14 +80,19 @@ public class RabbitMQAutoConnection {
          factory.setUsername(this.login);
          factory.setPassword(this.password);
 
-         this.connection = factory.newConnection(); // TODO: call newConnextion with null, Address[] ?
+         try {
+            this.connection = factory.newConnection(); // TODO: call newConnection with null, Address[] ?
+         } catch (ConnectException ignored) {
+         }
 
          try {
             Thread.sleep(this.interval);
          } catch (InterruptedException ignored) {
          }
       }
-      Logger.getLogger(RabbitMQAutoConnection.class.getName()).fine("Connected to the rabbitmq server.");
+      if (this.isConnected())
+         Logger.getLogger(RabbitMQAutoConnection.class.getName()).info("Connected to the rabbitmq server.");
+      // TODO else exception
    }
 
    public Connection getConnection() throws IOException {
@@ -91,13 +100,211 @@ public class RabbitMQAutoConnection {
       return this.connection;
    }
 
+   /**
+    * Wrapper around Connection methods
+    * Comments taken from Connection
+    */
+
+   /**
+    * Retrieve the host.
+    *
+    * @return the hostname of the peer we're connected to.
+    */
+   public InetAddress getAddress() throws IOException {
+      return this.getConnection().getAddress();
+   }
+
+   /**
+    * Retrieve the port number.
+    *
+    * @return the port number of the peer we're connected to.
+    */
+
+   public int getPort() throws IOException {
+      return this.getConnection().getPort();
+   }
+
+   /**
+    * Get the negotiated maximum channel number. Usable channel
+    * numbers range from 1 to this number, inclusive.
+    *
+    * @return the maximum channel number permitted for this connection.
+    */
+   public int getChannelMax() throws IOException {
+      return this.getConnection().getChannelMax();
+   }
+
+   /**
+    * Get the negotiated maximum frame size.
+    *
+    * @return the maximum frame size, in octets; zero if unlimited
+    */
+   public int getFrameMax() throws IOException {
+      return this.getConnection().getFrameMax();
+   }
+
+   /**
+    * Get the negotiated heartbeat interval.
+    *
+    * @return the heartbeat interval, in seconds; zero if none
+    */
+   public int getHeartbeat() throws IOException {
+      return this.getConnection().getHeartbeat();
+   }
+
+   /**
+    * Get a copy of the map of client properties sent to the server
+    *
+    * @return a copy of the map of client properties
+    */
+   public Map<String, Object> getClientProperties() throws IOException {
+      return this.getConnection().getClientProperties();
+   }
+
+   /**
+    * Retrieve the server properties.
+    *
+    * @return a map of the server properties. This typically includes the product name and version of the server.
+    */
+   public Map<String, Object> getServerProperties() throws IOException {
+      return this.getConnection().getServerProperties();
+   }
+
+   /**
+    * Create a new channel, using an internally allocated channel number.
+    *
+    * @return a new channel descriptor, or null if none is available
+    * @throws IOException if an I/O problem is encountered
+    */
    public Channel createChannel() throws IOException {
       return this.getConnection().createChannel();
    }
 
+   /**
+    * Create a new channel, using the specified channel number if possible.
+    *
+    * @param channelNumber the channel number to allocate
+    * @return a new channel descriptor, or null if this channel number is already in use
+    * @throws IOException if an I/O problem is encountered
+    */
+   public Channel createChannel(int channelNumber) throws IOException {
+      return this.getConnection().createChannel(channelNumber);
+   }
+
+   /**
+    * Close this connection and all its channels
+    * with the {@link com.rabbitmq.client.AMQP#REPLY_SUCCESS} close code
+    * and message 'OK'.
+    * <p/>
+    * Waits for all the close operations to complete.
+    *
+    * @throws IOException if an I/O problem is encountered
+    */
    public void close() throws IOException {
-      if (this.connection != null && this.connection.isOpen()) {
-         this.connection.close();
-      }
+      this.getConnection().close();
+   }
+
+   /**
+    * Close this connection and all its channels.
+    * <p/>
+    * Waits for all the close operations to complete.
+    *
+    * @param closeCode    the close code (See under "Reply Codes" in the AMQP specification)
+    * @param closeMessage a message indicating the reason for closing the connection
+    * @throws IOException if an I/O problem is encountered
+    */
+   public void close(int closeCode, String closeMessage) throws IOException {
+      this.getConnection().close(closeCode, closeMessage);
+   }
+
+   /**
+    * Close this connection and all its channels
+    * with the {@link com.rabbitmq.client.AMQP#REPLY_SUCCESS} close code
+    * and message 'OK'.
+    * <p/>
+    * This method behaves in a similar way as {@link #close()}, with the only difference
+    * that it waits with a provided timeout for all the close operations to
+    * complete. When timeout is reached the socket is forced to close.
+    *
+    * @param timeout timeout (in milliseconds) for completing all the close-related
+    *                operations, use -1 for infinity
+    * @throws IOException if an I/O problem is encountered
+    */
+   public void close(int timeout) throws IOException {
+      this.getConnection().close(timeout);
+   }
+
+   /**
+    * Close this connection and all its channels.
+    * <p/>
+    * Waits with the given timeout for all the close operations to complete.
+    * When timeout is reached the socket is forced to close.
+    *
+    * @param closeCode    the close code (See under "Reply Codes" in the AMQP specification)
+    * @param closeMessage a message indicating the reason for closing the connection
+    * @param timeout      timeout (in milliseconds) for completing all the close-related
+    *                     operations, use -1 for infinity
+    * @throws IOException if an I/O problem is encountered
+    */
+   public void close(int closeCode, String closeMessage, int timeout) throws IOException {
+      this.getConnection().close(closeCode, closeMessage, timeout);
+   }
+
+   /**
+    * Abort this connection and all its channels
+    * with the {@link com.rabbitmq.client.AMQP#REPLY_SUCCESS} close code
+    * and message 'OK'.
+    * <p/>
+    * Forces the connection to close.
+    * Any encountered exceptions in the close operations are silently discarded.
+    */
+   public void abort() throws IOException {
+      this.getConnection().abort();
+   }
+
+   /**
+    * Abort this connection and all its channels.
+    * <p/>
+    * Forces the connection to close and waits for all the close operations to complete.
+    * Any encountered exceptions in the close operations are silently discarded.
+    *
+    * @param closeCode    the close code (See under "Reply Codes" in the AMQP specification)
+    * @param closeMessage a message indicating the reason for closing the connection
+    */
+   public void abort(int closeCode, String closeMessage) throws IOException {
+      this.getConnection().abort(closeCode, closeMessage);
+   }
+
+   /**
+    * Abort this connection and all its channels
+    * with the {@link com.rabbitmq.client.AMQP#REPLY_SUCCESS} close code
+    * and message 'OK'.
+    * <p/>
+    * This method behaves in a similar way as {@link #abort()}, with the only difference
+    * that it waits with a provided timeout for all the close operations to
+    * complete. When timeout is reached the socket is forced to close.
+    *
+    * @param timeout timeout (in milliseconds) for completing all the close-related
+    *                operations, use -1 for infinity
+    */
+   public void abort(int timeout) throws IOException {
+      this.getConnection().abort(timeout);
+   }
+
+   /**
+    * Abort this connection and all its channels.
+    * <p/>
+    * Forces the connection to close and waits with the given timeout
+    * for all the close operations to complete. When timeout is reached
+    * the socket is forced to close.
+    * Any encountered exceptions in the close operations are silently discarded.
+    *
+    * @param closeCode    the close code (See under "Reply Codes" in the AMQP specification)
+    * @param closeMessage a message indicating the reason for closing the connection
+    * @param timeout      timeout (in milliseconds) for completing all the close-related
+    *                     operations, use -1 for infinity
+    */
+   public void abort(int closeCode, String closeMessage, int timeout) throws IOException {
+      this.getConnection().abort(closeCode, closeMessage, timeout);
    }
 }
